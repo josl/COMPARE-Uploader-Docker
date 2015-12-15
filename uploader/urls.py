1bdee181fe02ad8098ef177dc13806783752c637
josl <bellod.cisneros@gmail.com>
2015-12-15 21:46:58 +0100

    uploader removed from gitignore

---
+++ uploader/urls.py
@@ -0,0 +1,40 @@
+from django.conf import settings
+from django.conf.urls import patterns, url, include
+from django.contrib import admin
+from api.views import (
+    ChunkedUploadDemo, MyChunkedUploadCompleteView,
+    SaveView, ChunkedUploadedSize
+)
+
+from meta.views import SendData, SendFile, SaveMeta
+
+admin.autodiscover()
+
+urlpatterns = patterns(
+    '',
+    url(r'^/?$',
+        ChunkedUploadDemo.as_view(), name='chunked_upload'),
+
+    url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^login/', 'jwt_auth.views.obtain_jwt_token'),
+
+    url(r'^api/save/?$',
+        MyChunkedUploadCompleteView.as_view(),
+        name='api_chunked_upload_complete'),
+    url(r'^api/chunks/?$', SaveView.as_view(),
+        name='api_chunked_upload_save'),
+    url(r'^api/size/?$', ChunkedUploadedSize.as_view(),
+        name='api_chunked_upload_size'),
+
+    url(r'^api/data/?$', SendData.as_view(),
+        name='api_chunked_upload_data'),
+    url(r'^api/meta/save/?$', SaveMeta.as_view(),
+        name='api_chunked_upload_data'),
+    url(r'^api/file/?$', SendFile.as_view(),
+        name='api_chunked_upload_data'),
+
+    url(r'^static/(.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT, 'show_indexes': False}),
+)
