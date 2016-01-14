b175e0b9858532ad10865253c8be52951bb1ab3d
josl <bellod.cisneros@gmail.com>
2016-01-14 16:31:15 +0100

    Token expires after 1 day

--- uploader/settings.py
+++ uploader/settings.py
@@ -143,4 +143,5 @@ REST_FRAMEWORK = {
 JWT_AUTH = {
     'JWT_ALLOW_REFRESH': True,
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=1),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1)
 }
