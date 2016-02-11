117868d699e00910355172217dd8e27e008718b9
josl <bellod.cisneros@gmail.com>
2016-02-11 14:03:50 +0100

    Django static file folder fixed

--- uploader/settings.py
+++ uploader/settings.py
@@ -16,7 +16,7 @@ import datetime
 from django.conf import settings
 from .settings import *
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -113,9 +113,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/templates/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = 'templates/'
+STATIC_ROOT = 'static/'
+
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'templates'), 
+]
 
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
